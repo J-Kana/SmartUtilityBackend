9a227ec210945acbf2e78a29a1df04b8d1200f53
KaNa <47849640+Kanatbek21@users.noreply.github.com>
2021-10-20 14:27:13 +0500

    Add files via upload

---
+++ corsConfig.js
@@ -0,0 +1,14 @@
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        }
+         else {
+            callback(new Error('Not allowed by CORS'))
+        }
+        callback(null, true)
+
+    }
+}
+module.exports = corsOptions
